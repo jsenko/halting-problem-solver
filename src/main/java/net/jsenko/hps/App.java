105e34458c4d99af7d0a65afd2374dbd027bef27
Jakub Senko <jsenko@redhat.com>
2016-04-12 11:22:11 +0200

    Empty mvn project.

---
+++ src/main/java/net/jsenko/hps/App.java
@@ -0,0 +1,13 @@
+package net.jsenko.hps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Yes! This TM halts indeed." );
+    }
+}
